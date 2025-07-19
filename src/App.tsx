7536453993723c2b78f0b6a98cf84df36b67477a
Mongark <mongarkprogrammer@gmail.com>
2025-07-19 13:21:35 -0400

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
 import './App.css'
 
+interface CardProps {
+  title: string;
+  description: string;
+  content: any;
+}
+
+function Card(props: CardProps) {
+  return(
+    <div className="card">
+      <h2>{ props.title       }</h2>
+      <h5>{ props.description }</h5>
+      { props.content     }
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
-      <div>
-      	<h1>Geo Principe - Portfolio</h1>
+      <div className="header">
+        <h1>Geo Principe</h1>
+        <p>Web Developer</p>
+      </div>
 
-	<h4>Hello and welcome to my personal website.</h4>
+      <div className="navbar">
+        <a href="#">Home</a>
+      </div>
+
+      <div className="row">
+        <div className="leftcolumn">
+          <Card title="Test" description="123" content={
+            <>
+              <p>Hellow</p> <br/>
+              <p>Hellow</p> <br/>
+              <p>Hellow</p>
+            </>
+          } />
+        </div>
+        <div className="rightcolumn">
+          <Card title="Test" description="123" content={
+            <>
+              <p>Hellow</p> <br/>
+              <p>Hellow</p> <br/>
+              <p>Hellow</p>
+            </>
+          } />
+        </div>
       </div>
     </>
   )
